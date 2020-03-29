202b086ad337b5bd40ce7d14c0a4b7588be3ed2b
Ariel Shabi <4ariel6@gmail.com>
2020-03-29 11:58:54 +0300

    Added user reducer and action

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import messages from './messageReducer';
+import user from './userReducer';
 
 export default combineReducers({
-    messages
+    messages,
+    user
 });
