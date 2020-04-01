6859678838222ce22c0143d11c92e14167c2a11f
ariel shabi <4ariel6@gmail.com>
2020-04-01 12:12:26 +0300

    added the web socket initator to app js

--- client/src/components/ChatWindow/ChatWindow.jsx
+++ client/src/components/ChatWindow/ChatWindow.jsx
@@ -1,23 +1,14 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import CreateMessagePanel from '../CreateMessagePanel';
 import MessageListPanel from '../MessageListPanel';
 
-const ChatWindow = ({ connectWebSocket }) => {
-    useEffect(() => {
-        connectWebSocket();
-    });
-
+const ChatWindow = () => {
     return (
         <div>
             <MessageListPanel />
             <CreateMessagePanel />
         </div>
     );
-};
-
-ChatWindow.propTypes = {
-    connectWebSocket: PropTypes.func.isRequired
 };
 
 export default ChatWindow;
