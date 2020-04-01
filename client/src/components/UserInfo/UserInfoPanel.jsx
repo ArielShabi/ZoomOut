f8bbec3801ea1cc5755021920479fd0a30707e0a
ariel shabi <4ariel6@gmail.com>
2020-04-02 00:15:19 +0300

    added MUI

--- client/src/components/UserInfo/UserInfoPanel.jsx
+++ client/src/components/UserInfo/UserInfoPanel.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { IconButton } from '@material-ui/core';
+import { Casino } from '@material-ui/icons';
 import './user-info-panel.css';
+
 
 const UserInfoPanel = ({ userName }) => (
     <div className="user-info-panel">
@@ -7,6 +10,9 @@ const UserInfoPanel = ({ userName }) => (
             <span> Your Name:</span>
             <br />
             <span> {userName}</span>
+            <IconButton>
+                <Casino />
+            </IconButton>
         </p>
     </div>
 );
