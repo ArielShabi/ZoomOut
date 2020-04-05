9368e09dc45ea97f265afd56c2333d11cc9e8021
ariel shabi <4ariel6@gmail.com>
2020-04-05 23:58:04 +0300

    update all other users when name changes

--- server/webSocketServer/messageHandlers/randomNameMessageHandler.js
+++ server/webSocketServer/messageHandlers/randomNameMessageHandler.js
@@ -1,5 +1,5 @@
 const { getRandomNameMessageType } = require('../messageTypes');
-const { createEditUserInfoMessage } = require('../messageCreator');
+const { createEditUserInfoMessage, createOtherUserEditedInfoMessage } = require('../messageCreator');
 const { getRandomName } = require('../../utils');
 
 const handleMessage = (message, user, userContainer) => {
@@ -12,6 +12,12 @@ const handleMessage = (message, user, userContainer) => {
     userContainer.editUser(user.id, newInfo);
     const editUserMessage = createEditUserInfoMessage(newInfo);
     user.connection.send(editUserMessage);
+
+    const newNameMessage = createOtherUserEditedInfoMessage(user.id, { name: newName });
+
+    userContainer.getAllOpenUsers(user.id).forEach(otherUser => {
+        otherUser.connection.send(newNameMessage);
+    });
 };
 
 module.exports = handleMessage;
